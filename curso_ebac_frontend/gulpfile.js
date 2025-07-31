4572388575c4bdca18fa25d64d8054d6cd5515d4
Lucas Lima Brito <79551410+LucasLBrito@users.noreply.github.com>
2025-07-31 11:06:24 -0300

    Refactor project structure and update styles; remove unused files and implement image carousel with Bootstrap

--- curso_ebac_frontend/gulpfile.js
+++ curso_ebac_frontend/gulpfile.js
@@ -3,6 +3,7 @@ const sourcemaps = require('gulp-sourcemaps');
 const sass = require('gulp-sass')(require('sass'));
 const uglify = require('gulp-uglify');
 const imagemin = require('gulp-imagemin');
+const inject = require('gulp-inject');
 
 
 function compileSass() {
@@ -10,7 +11,7 @@ function compileSass() {
     .pipe(sourcemaps.init())
     .pipe(sass().on('error', sass.logError))
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist/css'));
+    .pipe(gulp.dest('dist/style'));
 }
 function minifyJs() {
   return gulp.src('src/scripts/**/*.js')
@@ -20,20 +21,31 @@ function minifyJs() {
       console.error('Error in compress task', err.toString());
     })
     .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('dist/js'));
+    .pipe(gulp.dest('dist/scripts'));
 }
 function compressImages() {
   return gulp.src('src/img/**/*')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'));
 }
-function copiHtml() {
-  return gulp.src('index.html')
-    .pipe(gulp.dest('dist'));
+// Tarefa para injetar o CSS no HTML
+function injetaHTML() {
+    // Pega os arquivos CSS e JS que serão injetados
+    const sources = gulp.src(['dist/style/*.css', 'dist/scripts/*.js'], { read: false });
+
+    // Pega o arquivo HTML principal
+    return gulp.src('src/index.html')
+        .pipe(inject(sources, { relative: true })) // Injeta os arquivos com caminho relativo
+        .pipe(gulp.dest('dist')); // Salva o novo index.html na pasta de destino
 }
 
 
 exports.compileSass = compileSass;
 exports.miniJs = minifyJs;
 exports.compressImages = compressImages;
-exports.copiHtml = copiHtml;
+exports.HTML = injetaHTML;
+exports.default = gulp.series(
+    compileSass,
+    minifyJs,
+    compressImages,
+    injetaHTML);
